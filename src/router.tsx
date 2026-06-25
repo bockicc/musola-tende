aa9617fdd2795ceb7c7112bd9d1e30c5d875a703
Veljko Bocić <veljkobocic@gmail.com>
2026-06-25 21:10:05 +0200

    Add basepath to TanStack Router for sub-path deployment

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { routeTree } from './routeTree.gen'
 
 export const router = createRouter({
   routeTree,
+  basepath: '/musola-tende',
   defaultPreload: 'intent',
   defaultStaleTime: 5000,
 })
